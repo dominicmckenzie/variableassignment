8974fcbb9df08f5e1bb893851b026e38a2561698
Dominic Mck <domosai@Dominics-MacBook-Pro.local>
2018-03-16 16:33:04 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var grant = 1000;
+var stateFunding = {
+  ngo: ["Help Unlimited", "New Start", "Better Tomorrow" , "Home Safe" , "New Home"]
+  estimatedExpenses: [800, 950, 1000, 1100, 1200]
+}
+
+var single= 0;
+var couple= 1;
+var fam= {
+  number: [2, 3, 4]
+};
